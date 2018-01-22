72963568e040b1320d95c7503265e81a0df47ce3
Shrikant Goswami <shrikant.goswami@gmail.com>
2018-01-23 01:53:30 +0530

    Upgrade Quorum to v2.0.0

---
+++ dynamic-node/templates/start-node.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#
+# This is used at Container start up to run the constellation and geth nodes
+#
+
+set -u
+set -e
+
+### Configuration Options
+TMCONF=/qdata/tm.conf
+
+GETH_ARGS="--datadir /qdata/dd --raft --rpc --rpcaddr 0.0.0.0 --rpcapi admin,db,eth,debug,miner,net,shh,txpool,personal,web3,quorum --nodiscover --unlock 0 --password /qdata/passwords.txt"
+
+if [ ! -d /qdata/dd/geth/chaindata ]; then
+  echo "[*] Mining Genesis block"
+  /usr/local/bin/geth --datadir /qdata/dd init /qdata/genesis.json
+fi
+
+echo "[*] Starting Constellation node"
+nohup /usr/local/bin/constellation-node $TMCONF 2>> /qdata/logs/constellation.log &
+
+sleep 2
+
+echo "[*] Starting node"
+PRIVATE_CONFIG=$TMCONF nohup /usr/local/bin/geth $GETH_ARGS 2>>/qdata/logs/geth.log
